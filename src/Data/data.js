e8cec6df842b234b60e89e019abde4194e3b9bdc
khokho <michealkosisochukwu300@gmail.com>
2024-10-23 16:36:25 +0100

    portfolio under construction

---
+++ src/Data/data.js
@@ -0,0 +1,107 @@
+export const projectData = [
+  {
+    name: 'Spotify App',
+    desc: 'Home for all musics',
+    lang: 'Typescript + ReactJs + MUI + TailwindCss',
+    link: 'https://ace-pacific.vercel.app/',
+  },
+  {
+    name: 'FoodBlog Web',
+    desc: 'A dummy template for a foodblog web',
+    lang: 'JavaScript + TypeScript + Css',
+    link: 'https://vercel.com/khokho/dark-readers-make/5bQjkcGb63MaxiQVgbAzTwALU8KV',
+  },
+  {
+    name: 'Rick & Morty WiKi',
+    desc: 'Rick and Morty Wiki is website that helps you get familiar with all characters and resource from the anime series Rick and Morty.',
+    lang: 'JavaScript + ReactJs + Bootstrap ',
+    link: 'https://rick-morty-clone-rho.vercel.app/',
+  },
+];
+  export const experienceData = [
+    {
+      role: 'Frontend Developer',
+      span: '2024 - till date',
+      },
+  ];
+  
+  export const skills = [
+    {
+      id: 0,
+      title: 'Html',
+    },
+    {
+      id: 1,
+      title: 'Css',
+    },
+    {
+      id: 2,
+      title: 'Bootstrap',
+    },
+    {
+      id: 3,
+      title: 'TailwindCSS',
+    },
+    {
+      id: 5,
+      title: 'JavaScript',
+    },
+    {
+      id: 6,
+      title: 'TypeScript',
+    },
+    {
+      id: 7,
+      title: 'Jquery',
+    },
+    {
+      id: 8,
+      title: 'React.Js',
+    },
+    {
+      id: 9,
+      title: 'Next.Js',
+    },
+    {
+      id: 10,
+      title: 'Git',
+    },
+    {
+      id: 11,
+      title: 'Github',
+    },
+    {
+      id: 12,
+      title: 'Figma',
+    },
+    {
+      id: 13,
+      title: 'Photoshop',
+    },
+  ];
+  
+
+  {/*{
+ {/* {
+    name: 'Shorty',
+    desc: 'Create short links, QR Codes, and Link-in-bio pages. Share them anywhere.Track what’s working, and what’s not',
+    lang: 'JavaScript + ReactJs + Html',
+    link: 'https://shorty-8jy8.onrender.com/',
+  },
+  {
+    name: 'Snappy',
+    desc: 'Designed and developed Snappy, a custom chat app for personal group chats...',
+    lang: 'ReactJs + NodeJs + ExpressJs + Bootstrap + MongoDB ',
+    link: 'https://snappy-frontend.onrender.com/',
+  },
+  {
+    name: 'YelpCalmp',
+    desc: 'A place to view all camping grounds close to you',
+    lang: 'NodeJs + ExpressJs + Bootstrap + MongoDB + EJS',
+    link: 'https://fine-erin-agouti-robe.cyclic.app/',
+  },
+    name: 'DALL-E 2.0',
+    desc: 'A search engine prototype that works like google',
+    lang: 'JavaScript + ReactJs + TailwindCss ',
+    link: 'https://client-git-master-samuel-ume.vercel.app/',
+  },*/}
